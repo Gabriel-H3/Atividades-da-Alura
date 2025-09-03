30abadc24a95e24abf78d99f418cc2f2e84ab6e5
Gabriel <gomesdemouracarneirogabriel@gmail.com>
2025-09-03 13:52:00 -0300

    Delete AtividadeAlura directory

--- AtividadeAlura/src/main/java/org/atividade/Perfil.java
+++
@@ -1,8 +0,0 @@
-package org.atividade;
-
-public class Perfil {
-    public static void main(String[] args) {
-        System.out.println("Olá, Gabriel");
-        System.out.println("Tudo bem?");
-    }
-}
